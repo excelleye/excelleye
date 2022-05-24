63347d5e54cfac77350bea188fd9c73fa2bbe3bd
Takumi ExcellEye <excelleye@gmail.com>
2022-05-24 18:00:43 +0900

    足し算プログラムを追加
    場所：portfolio/program/rust/addition

---
+++ portfolio/program/rust/addition/src/main.rs
@@ -0,0 +1,46 @@
+use std::io;
+use std::string::String;
+
+fn main() {
+    add();
+}
+
+//Addition Program
+fn add() {
+        println!("Addition program...");
+
+    //First numerical entry.
+        println!("Please enter the first number.");
+        let mut mystring = String::new();
+        io::stdin()
+            .read_line(&mut mystring)
+            .expect("Input error!");
+
+        let first_num : i64 = match mystring
+            .trim()
+            .parse(){
+                Ok(num) => num,
+                Err(_) => 0,
+            
+            };
+
+    //Second numerical entry.
+        println!("Please enter the second number.");
+        let mut mystring = String::new();
+        io::stdin()
+            .read_line(&mut mystring)
+            .expect("Input error!");
+        
+        let second_num : i64 = match mystring
+            .trim()
+            .parse(){
+                Ok(num) => num,
+                Err(_) => 0,
+            };
+    //Output of calculation results.
+        let answer =first_num + second_num;
+
+        println!("Calculation Results...");
+        println!("{} + {} = {}",first_num,second_num,answer);
+
+ }
